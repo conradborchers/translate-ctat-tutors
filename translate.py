0d87f4382bce87bf65345f9864db8228de49a31f
conradborchers <conrad.borchers@hotmail.de>
2022-10-06 20:57:39 -0400

    more robust brd translation via mass production (GB approach)

--- translate.py
+++ translate.py
@@ -2,7 +2,8 @@ from ctypes.util import find_library
 
 import glob
 import json
-import sys
+
+import xml.etree.ElementTree as ET
 
 from timeit import repeat
 
@@ -12,7 +13,15 @@ from tqdm import tqdm
 import translators as ts
 ts._google.language_map 
 
-PROJECT_DIR = '/home/<USER>/Desktop/translate-ctat-tutors/'
+import sys
+import xml.etree.ElementTree as ET
+
+import re
+from nltk.corpus import stopwords
+
+STOPWORDS = stopwords.words('english')
+
+PROJECT_DIR = '/home/cbo/Desktop/translate-ctat-tutors/'
 TRANSLATION_DICT = PROJECT_DIR+'translations-stoich-en-to-de.json'
 
 def load_translations(f=TRANSLATION_DICT):
@@ -63,85 +72,99 @@ if not SKIP_HTML:
         with open(outpath, 'w') as outfile:
             outfile.write(str(soup))
 
-# -- BRD --
+# -- BRD -- -> Mass-production variable placeholder strategy
+
+def clean_name(s, convert_to_lower=True):
+    s = re.sub('<[^<]+?>', '', s) #markup
+    s = re.sub('[^0-9a-zA-Z_\s]', '', s) #keep alnum
+    s = re.sub('\t\n\r', '', s) #remove tab, line break, carriage return
+    s = ' '.join(s.split()) #remove redundant whitespace
+    return s.lower() if convert_to_lower else s
+
+def clean_phrase(s, convert_to_lower=False):
+    # TODO (check if html references should be preserved in translation)
+    s = re.sub('<[^<]+?>', '', s) #markup
+    s = re.sub('[^0-9a-zA-Z_\'\s*/+-äöüÄÖÜß!?\.]', '', s) #keep alnum, operators, umlauts, and apostrophe
+    s = re.sub('\t\n\r', '', s) #remove tab, line break, carriage return
+    s = ' '.join(s.split()) #remove redundant whitespace
+    return s.lower() if convert_to_lower else s
+
+def make_var(phrase, signature='_', keep_n_words=4):
+    if(phrase is None or phrase == ''):
+        return ''
+    h = signature + '_' + '_'.join([word for word in clean_name(phrase).split(' ') if word not in STOPWORDS][:keep_n_words])
+    v = '%(' + str(h) + ')%'
+
+    the_clean_phrase = clean_phrase(phrase)
+    var_phrase_map[v] = the_clean_phrase
+
+    # Translate, translations will be stored with same key as English text in different dict()
+    if the_clean_phrase in repeated_translations:
+        var_translation_map[v] = repeated_translations[the_clean_phrase]
+    else:
+        try:
+            translation = ts.google(the_clean_phrase, from_language='en', to_language='de')
+            translation = clean_phrase(translation) # Clean translation
+            repeated_translations[the_clean_phrase] = translation
+            var_translation_map[v] = translation
+        except:
+            pass
+    return v
+
+def iterate_generic(tag: str, root):
+    """
+    TODO Review
+    Only translate first input (i.e., prolem statement) for now.
+    """
+    count = 1
+    for element in root.iter(tag):
+        if tag == 'Input' and count > 1:
+            continue
+        if tag == 'Input':
+            txt = element[0].text
+        else:
+            txt = element.text
+        if txt is None or txt.startswith("%("):
+            continue
+        else:
+            if tag == 'Input':
+                element[0].text = make_var(txt, signature=tag+'_'+str(count))
+            else:
+                element.text = make_var(txt, signature=tag+'_'+str(count))
+        count += 1
+
+def process_file(infile, outfile_brd, outfile_massprod):
+    global tree; tree = ET.parse(infile)
+    global root; root = tree.getroot()
+    global var_phrase_map; var_phrase_map = dict()
+    global var_translation_map; var_translation_map = dict()
+    
+    tags = ['hintMessage', 'buggyMessage', 'successMessage', 
+            'label', 'Input']
+    # tags += ['productionRule'] # exclude for now
+    for tag in tags:
+        iterate_generic(tag, root)
+    
+    tree.write(outfile_brd)
+    
+    signature = infile.split('/')[-1].split('.brd')[0]
+    with open(outfile_massprod, "w") as f:
+        f.write(f"{signature}\t{signature+'_english'}\t{signature+'_german'}\t\n")
+        for k in var_phrase_map.keys():
+            en = var_phrase_map[k]
+            de = var_translation_map.get(k)
+            if de is None:
+                de = '### FEHLENDE UEBERSERSETZUNG ###' # missing translation if API fails, currently no re-tries, need to hand-code 
+            else:
+                de = de.replace('\t', '') # TODO: Some translation remained with tabs for some reason
+            f.write(f"{k}\t{en}\t{de}\n")
+    
+    save_translations(repeated_translations)
+    return
+
 print("Transforming brd files...")
-
-# List BRD files
 fs_brd = glob.glob(PROJECT_DIR+'files/FinalBRDs/*')
-
-# BRD tags to translate
-elements = ['buggyMessage', 'hintMessage', 'successMessage', 'buggymessage', 'hintmessage', 'successmessage']
-
-# Problem statements
-# Problem statements are in value tags within input tags. However, if you replace all value tags
-# throughout BRD files will corrupt some references that should not be changed. Therefore, I 
-# match titles verbatim right now until I have found a better way of replacing them.
-problem_statements = [
-    "When you solve chemistry problems, you convert units to better",
-    "Let's convert liters (L) to milliliters (mL) in this problem.",
-    "Can you say how many grams of potatoes we would have?",
-    "The World Health Organization's (WHO) recommended limit for arsenic",
-    "Let's suppose we are responsible for a water supply that has become contaminated with alcohol (COH4).",
-    "Let's calculate the number of molecules in a single gram (1.00) of H2",
-    "Our result should have 3 significant figures",
-    "Our result should have 5 significant figures",
-    "Let's calculate the number of moles of Oxygen atoms in 15.95",
-    "Can we calculate the number of moles of salt (NaCl) that are in a 350 ml solutio",
-    "Let's say we have 125.0 mL of a Na3PO4 solution that has a concentration of 1.231",
-    "Suppose we just discovered that it is possible to produce H2 with renewable energy source",
-    "Let's figure out how many moles of O2 are in 1.216 kg COH4 and be sure our result has 4 significant figure"
-]
-
-# Run translation routine over all BRD files
-# We need a parsed soup object to identify the right tags to replace and then replace the source code
-# via string matching in order to preserve formating
-SKIP_BRD = False
-if not SKIP_BRD:
-    for f_str in fs_brd:
-        print(f'Running {f_str}...')
-        with open(f_str, 'r') as infile:
-            soup = BeautifulSoup(infile, 'html.parser')
-        with open(f_str, 'r') as infile:
-            raw = infile.read()
-        for found in tqdm(soup.findAll(elements)):
-            if found.string is None:
-                continue
-            if found.string in repeated_translations:
-                #raw = raw.replace(found.string.split('<a href')[0], repeated_translations[found.string]) # preserve hrefs, translator will omit them
-                raw = raw.replace(found.string, repeated_translations[found.string])
-            else:
-                try:
-                    translation = ts.google(found.string, from_language='en', to_language='de')
-                    repeated_translations[found.string] = translation
-                    #raw = raw.replace(found.string.split('<a href')[0], translation) # preserve hrefs, translator will omit them
-                    raw = raw.replace(found.string, translation)
-                except:
-                    continue
-        # Translate <value> inside <input> in separate loop to replace problem statements
-        for found in tqdm(soup.findAll('value')):
-            found_problem_statement = False
-            for ps in problem_statements:
-                try:
-                    if ps.lower() in found.string.lower():
-                        print('Found problem statement!')
-                        found_problem_statement = True
-                except:
-                    continue
-            if found_problem_statement:
-                if found.string in repeated_translations:
-                    #raw = raw.replace(found.string.split('<a href')[0], repeated_translations[found.string]) # preserve hrefs, translator will omit them
-                    raw = raw.replace(found.string, repeated_translations[found.string])
-                else:
-                    try:
-                        translation = ts.google(found.string, from_language='en', to_language='de')
-                        repeated_translations[found.string] = translation
-                        #raw = raw.replace(found.string.split('<a href')[0], translation) # preserve hrefs, translator will omit them
-                        raw = raw.replace(found.string, translation)
-                    except:
-                        continue
-            else:
-                continue
-        save_translations(repeated_translations)
-        outpath = f_str.replace('/files/FinalBRDs/', '/translated_files/FinalBRDs/').replace('.brd', '_german.brd')
-        with open(outpath, 'w') as outfile:
-            outfile.write(raw)
+for infile in tqdm(fs_brd):
+    outfile_brd = infile.replace('/files/FinalBRDs/', '/translated_files/FinalBRDs/').replace('.brd', '_placeholder.brd')
+    outfile_massprod = infile.replace('/files/FinalBRDs/', '/translated_files/mass_production/').replace('.brd', '_massproduction.txt')
+    process_file(infile, outfile_brd, outfile_massprod)
